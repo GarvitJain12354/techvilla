59a1e9b80cff970f21d44fa90f4ae25b9fde83c4
first <garvitjainraju@gmail.com>
2024-03-03 00:10:39 +0530

    codebeta

--- app/codebeta/page.js
+++ app/codebeta/page.js
@@ -24,8 +24,8 @@ const page = () => {
         id="page1"
         className=" div flex-col flex items-center justify-center h-screen  bg-black relative w-full overflow-hidden"
       >
-        <div className="blue "></div>
-        <div className="violet"></div>
+        <div className="blue max-md:z-0"></div>
+        <div className="violet max-md:z-0"></div>
 
         {dimensions.width > 0 && dimensions.height > 0 && (
           <Confetti
@@ -33,9 +33,10 @@ const page = () => {
             height={dimensions.height}
             recycle={false}
             numberOfPieces={500}
+            
           />
         )}
-        <video src="/codebeta.mp4" className="w-full h-full " autoPlay loop muted></video>
+        <video src="/codebeta.mp4" className="w-full h-full relative " autoPlay loop muted></video>
       </div>
     </>
   );
